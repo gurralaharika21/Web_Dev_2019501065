1ed7fcee5a27b0dd8c3a862e90af53caa72cabd2
unknown <Srii1020>
2020-04-20 00:53:01 +0530

    working on creating table for books

--- project1/models.py
+++ project1/models.py
@@ -16,3 +16,22 @@ class Users(db.Model):
         self.email = email
         self.password = password
         self.timestamp = timestamp
+
+''' class for books'''
+class Book(db.Model):
+    __tablename__ = "books"
+    id = db.Column(db.Integer,primary_key = True)
+    isbn = db.Column(db.String(100), unique = True,nullable = False)
+    title = db.Column(db.String(100), unique = False,nullable = False)
+    author = db.Column(db.String(128))
+    year = db.Column(db.Integer,nullable=False)
+
+
+    def __init__(self,isbn,title,author,year):
+        self.isbn = isbn
+        self.title = title
+        self.author = author
+        self.year = year    
+
+    def __repr__(self):
+        retyrn self.title    

2431bdd577b9047842efe675e2629d8e1f865e99
unknown <Srii1020>
2020-04-27 20:13:05 +0530

    Assignmnets on javascript

---
+++ javascript/Day1-Javascript/Assignment1.js
@@ -0,0 +1,32 @@
+const str = "Be good, Harika B"
+var string1 = "harika is learning javascript"
+var letter = 'i'
+
+function countBs(str1) {
+    count = 0;
+    for(let i = 0; i < str.length; i++) {
+        if(str[i] === "B") {
+            count++;
+        }
+
+    }
+    console.log(count)
+    // return count
+    }
+
+countBs(str)    
+
+function countChar(string,letter) {
+    count = 0;
+    letter = 'h'
+    for(let i = 0; i<string.length;i++) {
+        if(string[i] === letter) {
+            count++;
+        }
+    }
+    console.log(count)
+    
+}
+countChar(string1,letter)
+
+
